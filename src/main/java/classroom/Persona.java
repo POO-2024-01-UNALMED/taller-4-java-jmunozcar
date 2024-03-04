3c09dab123d035b1894655bec6d583f341c4c598
jmunozcar <jmunozcar@unal.edu.co>
2024-03-04 14:24:23 -0500

    Intento 1

--- src/main/java/classroom/Persona.java
+++ src/main/java/classroom/Persona.java
@@ -2,15 +2,19 @@ package classroom;
 
 public class Persona {
 
-    final long cedula;
-    String nombre;
+    long cedula = 1;
+    private String nombre;
     static int totalPersonas;
     
     static {
         totalPersonas = 0;
-        cedula = 3;
     }
 
+    public Persona() {
+        this.cedula = 0;
+        totalPersonas++;
+    }
+    
     public Persona(long cedula, String nombre) {
         this.cedula = cedula;
         this.nombre = nombre;
@@ -30,7 +34,7 @@ public class Persona {
     }
 
     public Persona(String nombre) {
-        this.nombre = "";
+        this.nombre = nombre;
         totalPersonas++;
     }
     
